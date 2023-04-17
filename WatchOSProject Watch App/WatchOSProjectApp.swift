1845b8c8e702578c1f6cf043f6ec24f334b4d86b
Vicwell28 <vicwell.bj@gmail.com>
2023-04-17 11:33:16 -0600

    init

---
+++ WatchOSProject Watch App/WatchOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSProjectApp.swift
+//  WatchOSProject Watch App
+//
+//  Created by soliduSystem on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSProject_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
